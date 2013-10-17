dbbdd11bdd4ff993a0631ac5249d5a179616820a
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-17 17:37:03 -0500

    added validations for candidate

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -3,5 +3,13 @@ class Candidate < User
   has_many :responses, through: :application
 
   has_many :nominations
+  validates :phone, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :address1, presence: true
 
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, :with => /.+@.+\..{2,}/
 end
