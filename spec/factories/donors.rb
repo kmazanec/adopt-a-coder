02dd5a7ba2abcacbfc6585aa600caae50940e0ab
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-17 17:44:35 -0500

    created factories and tests for donors

---
+++ spec/factories/donors.rb
@@ -0,0 +1,30 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+FactoryGirl.define do
+  factory :donor do |f|
+      f.email "ka@gmail.com"
+      f.name "Kevin Another"
+      f.phone "555-212-6061"
+      f.password "Password"
+      f.password_confirmation "Password"
+      f.address1 "225 north illinois"
+      f.address2 "apt 60602"
+      f.city "Chicago"
+      f.state "IL"
+      f.zip "06061"
+      f.twitter "scott@twitter.com"
+      f.facebook "scott@facebook.com"
+      f.linked_in "scott@linked in"
+      f.codeacademy "badges"
+      f.github "scott@github.com"
+      f.blog "tumblerme@tumblr.com"
+      f.personal_url "scott@tumblr.com"
+      f.currently_working_on "code academy"
+      f.biography "from the north side"
+      f.mission "become a great coder"
+      f.profile_photo_id "1"
+      f.profile_video_id "1"
+      f.type "Donor"
+
+
+  end
+end
