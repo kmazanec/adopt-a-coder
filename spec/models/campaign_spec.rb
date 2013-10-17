056e579f61b2e6f928f7043c134709a2e4a9e024
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-17 17:43:04 -0500

    validation and association tests for campaign

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
-describe Campaign do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe Campaign do
+
+    context "Validations and Associations" do
+
+      it "should belong to a candidate" do
+        campaign = Campaign.reflect_on_association(:candidate)
+        campaign.macro.should == :belongs_to
+      end
+
+      it "should have many donations" do
+        campaign = Campaign.reflect_on_association(:donations)
+        campaign.macro.should == :has_many
+      end
+
+      it "should have many donors" do
+        campaign = Campaign.reflect_on_association(:donors)
+        campaign.macro.should == :has_many
+      end
+
+      it "should have many nominations" do
+        campaign = Campaign.reflect_on_association(:nominations)
+        campaign.macro.should == :has_many
+      end
+
+      it "should have a start date" do
+         FactoryGirl.build(:campaign, start_date: nil).should_not be_valid
+      end
+
+      it "should have an end date" do
+         FactoryGirl.build(:campaign, end_date: nil).should_not be_valid
+      end
+
+      it "should not need to have a total" do
+         FactoryGirl.build(:campaign, total: nil).should be_valid
+      end
+
+      it "should need to have a goal" do
+         FactoryGirl.build(:campaign, goal: nil).should_not be_valid
+      end
+
+
+    end
+  end
+
