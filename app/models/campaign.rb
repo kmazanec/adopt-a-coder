11ae54ddd7bc733233067e22a2d2849ca6d4bf2f
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-17 17:36:30 -0500

    added validations for campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,6 +2,8 @@ class Campaign < ActiveRecord::Base
   belongs_to :candidate
   has_many :donations
   has_many :donors, through: :donations
-  
+
   has_many :nominations
+  validates :goal, :end_date, :start_date, presence: true
+
 end
