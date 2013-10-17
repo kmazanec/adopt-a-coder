472de8072e0a83c9b5e5fc4a61c6de6e3473888e
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-17 18:52:51 -0500

    Main page 'about' completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,7 @@
 module ApplicationHelper
 
   def current_campaign
-    @current_campaign ||= Campaign.find(1)
-  end
-
-  def current_campaign_candidate
-
-
+    @current_campaign ||= Campaign.includes(:candidate).find(1)
   end
 
 end
