b3c4b58fb7cfa4986efb4cfe0f74888c9e2a50da
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-17 12:44:16 -0500

    made factories for all models

--- spec/factories/donations.rb
+++ spec/factories/donations.rb
@@ -1,6 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :donation do
+  factory :donation do |f|
+    f.token "02348210485724"
+    f.amount "20"
+    f.donor_id "3"
+    f.campaign_id "2"
+
   end
 end
+
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,6 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
-  factory :question do
+  factory :question do |f|
+    f.body "WHat is the purpose of life"
   end
 end
