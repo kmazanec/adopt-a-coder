6ee3b6d53397efc6e79d63f727f468289a4bfbdd
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-17 14:18:13 -0500

    created model and assciation tests for media

--- spec/models/media_spec.rb
+++ spec/models/media_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
-describe Media do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe Media do
+
+    context "Validations and Associations" do
+
+      it "should belong to a user" do
+        media = Media.reflect_on_association(:user)
+        media.macro.should == :belongs_to
+      end
+
+      it "should have a link to a video of themselves" do
+        FactoryGirl.create(:media).should be_valid
+      end
+
+      it "should not be valid unless they have a link to a video or picture of themselves" do
+        FactoryGirl.create(:media, url: nil).should_not be_valid
+      end
+
+
+
+
+    end
+  end
+
