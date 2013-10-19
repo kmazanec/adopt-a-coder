a3efa3710f7943e21f0b857c66428522f99120f2
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-19 16:07:46 -0500

    candidate profile page integration page

---
+++ spec/features/candidate_profile_page_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+include UserHelper
+
+describe "Profile Page for candidate" do
+
+  before(:each) do
+    login_as_candidate
+  end
+
+
+  it "should let you start your application" do
+
+
+    page.should have_link "Application Form"
+  end
+
+  it "should let you sumbit your application" do
+
+
+    page.should have_link "Submit Application"
+  end
+
+  it "should show copyright" do
+
+    within('footer') { expect(page).to have_content('Copyright') }
+  end
+
+
+end
