0793a0054d3fbb8a40efca61eea3380c4a43042b
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-19 16:09:20 -0500

    candidate sign up integration tests helper

--- spec/support/user.helper.rb
+++ spec/support/user.helper.rb
@@ -13,4 +13,23 @@ module UserHelper
       click_button "Create Donor"
   end
 
+  def login_as_candidate
+    visit root_path
+    click_link "Sign Up!"
+    click_link "Interested in applying?"
+
+      fill_in 'Name', with: 'Derrick Rose'
+      fill_in 'candidate_email', with: 'derrickrose@gmail.com'
+      fill_in 'candidate_phone', with: '123-565-1897'
+      fill_in 'candidate_password', with: 'numberone'
+      fill_in 'candidate_password_confirmation', with: 'numberone'
+      fill_in 'candidate_address1', with: 'trump tower'
+      fill_in 'candidate_address2', with: 'englewood'
+      fill_in 'candidate_city', with: 'chicago'
+      fill_in 'candidate_state', with: 'IL'
+      fill_in 'candidate_zip', with: '60601'
+
+      click_button "Create Profile"
+  end
+
 end
