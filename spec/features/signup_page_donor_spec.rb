d3261620d6ca02d47f0624422170b09b5d05310f
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-19 16:09:55 -0500

    sign up integration tests for donor

---
+++ spec/features/signup_page_donor_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+include UserHelper
+
+describe "Sign Up Page for donor" do
+
+  it "should let you sign up as a donator" do
+    visit root_path
+    click_link "Sign Up!"
+    click_link "Interested in being a supporter?"
+
+      fill_in 'Name', with: 'Scott'
+      fill_in 'donor_email', with: 'pippen@gmail.com'
+      fill_in 'donor_password', with: 'bah'
+      fill_in 'donor_password_confirmation', with: 'bah'
+
+    click_button "Create Donor"
+    page.should have_content "Candidates You've Nominated"
+    page.should have_content "Scott"
+
+  end
+
+  it "should show the copyright" do
+    visit root_path
+    click_link "Sign Up!"
+    click_link "Interested in being a supporter?"
+
+    within('footer') { expect(page).to have_content('Copyright') }
+  end
+
+
+
+
+
+end
