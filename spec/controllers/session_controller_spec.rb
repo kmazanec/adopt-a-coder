18337ed85e7aed2abd80bd3abe5cf5f459cf2c39
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-19 12:00:19 -0500

    login tests for session controller

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -4,7 +4,7 @@ describe SessionsController do
 
   describe '#logout' do
 
-    before do
+    before(:each) do
 
       @candidate = FactoryGirl.create(:candidate)
     end
@@ -26,10 +26,40 @@ describe SessionsController do
 
       redirect_to root_path
     end
-
-
-
   end
 
+  describe '#login' do
+
+    before(:each) do
+      @user = Candidate.create!(email: "user1@user.com", password: "password", password_confirmation: "password", city: "chicago", phone: "123-456-1234", state: "nj", zip: "6-60101", address1: "this place")
+      @donor = Donor.create!(email: "useragfe1@user.com", password: "password", password_confirmation: "password", city: "chicagos", phone: "123-456-5234", state: "Il", zip: "60101", address1: "this place is awesome")
+
+    end
+
+    it "should set the session id to the to the user id" do
+      post :login, { email: @user.email, password: "password" }
+
+      expect(session[:id]).to eq @user.id
+    end
+
+    it "should redirect to the candidates profile page if user is a candidate" do
+      post :login, { email: @user.email, password: "password" }
+
+      response.should redirect_to(candidate_path(@user))
+    end
+
+    it "should redirect to the donor profile page if user a donor" do
+      post :login, { email: @donor.email, password: "password" }
+
+      response.should redirect_to(donor_path(@donor))
+    end
+
+    it "should flash notice if the email or password is incorrect" do
+      post :login, { email: @user.email, password: "password55" }
+
+      flash[:notice].should_not be_nil
+      response.should redirect_to(root_path)
+    end
+  end
 
 end
