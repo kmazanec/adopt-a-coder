f92933a12a2fecb9dc3aceea4feb7938b9866c01
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-20 16:37:05 -0500

    nomination available test

--- spec/models/donor_spec.rb
+++ spec/models/donor_spec.rb
@@ -41,11 +41,13 @@ require 'spec_helper'
         FactoryGirl.build(:donor, password: nil).should_not be_valid
       end
 
-      it "should check to see if there are nominations avaliable" do
-        donor = FactoryGirl.build(:donor)
-        available= donor.nomination_available?
+      describe "#nomination_available?" do
+        it "should check to see if there are nominations avaliable" do
+          donor = FactoryGirl.build(:donor)
+          available= donor.nomination_available?
 
-        available.should eq true
+          available.should eq true
+        end
       end
 
     end
