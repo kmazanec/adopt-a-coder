7a5f7828982b4a220d796145de1c736b59ec3c64
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-19 20:27:47 -0500

    added a callback function on the donation that autopopulates the campaign total amount of donations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -6,4 +6,13 @@ class Donation < ActiveRecord::Base
   validates :token, presence: true
   validates :token, uniqueness: true
   validates :amount, numericality: { :greater_than_or_equal_to => 10 }
+
+  after_create :update_donations
+
+  def update_donations
+    campaign = Campaign.find(self.campaign_id)
+    total = campaign.donations.sum(:amount)
+    campaign.update_attributes(total: total)
+  end
+
 end
