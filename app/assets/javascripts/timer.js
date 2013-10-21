c05836eaf463374411e71ffecffc543c34113eb3
kmazanec <k.mazanec@gmail.com>
2013-10-21 17:16:55 -0500

    style candidate profile create and other things

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.countdown').downCount({
-    date: '12/31/2013 12:00:00',
+    date: '10/25/2013 12:00:00',
     offset: -6
   }, function () {
     alert('WOOT WOOT, done!');
