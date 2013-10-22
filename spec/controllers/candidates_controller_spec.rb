75308617ae01a73728795f73346751a914c5c414
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-22 13:54:42 -0500

    refactored tests

--- spec/controllers/candidates_controller_spec.rb
+++ spec/controllers/candidates_controller_spec.rb
@@ -2,84 +2,60 @@ require 'spec_helper'
 
 describe CandidatesController do
 
-   before(:each) do
-     @candidate = FactoryGirl.create(:candidate)
-   end
 
   describe "#create" do
-
-    it 'should be an instatance of candidate' do
-
-      @candidate.should be_an_instance_of Candidate
+    before do
+      @params = { "email" => "scott@gmail.com",
+                  "name" => "Scott M", "phone" => "555-233-6541","password" => "Password1",
+                  "password_confirmation" => "Password1", "address1" => "224 north illinois",
+                  "address2" => "apt 6002", "city" => "Chicago", "state" => "IL", "zip" => "06061",
+                  "twitter" => "kevin@twitter.com", "facebook" => "kevin@facebook.com",
+                  "linked_in" => "kevin@linked in", "codeacademy" => "badges", "github" => "kevin@github.com",
+                  "blog" => "tumblerkevin@tumblr.com", "personal_url" => "kevin@tumblr.com",
+                  "currently_working_on" => "codes  academy", "biography" => "from the north side yo",
+                  "mission" => "become a great coder too", "profile_photo_id" => "2",
+                  "profile_video_id" => "2", "type" => "Candidate"}
     end
 
-    it "should be able to create a new candidate" do
+    context "valid params" do
+      before do
+        post 'create', :candidate => @params
 
-      expect { post 'create', :candidate => { "email" => "scott@gmail.com",
-        "name" => "Scott M", "phone" => "555-233-6541","password" => "Password1",
-        "password_confirmation" => "Password1", "address1" => "224 north illinois",
-        "address2" => "apt 6002", "city" => "Chicago", "state" => "IL", "zip" => "06061",
-        "twitter" => "kevin@twitter.com", "facebook" => "kevin@facebook.com",
-        "linked_in" => "kevin@linked in", "codeacademy" => "badges", "github" => "kevin@github.com",
-        "blog" => "tumblerkevin@tumblr.com", "personal_url" => "kevin@tumblr.com",
-        "currently_working_on" => "codes  academy", "biography" => "from the north side yo",
-        "mission" => "become a great coder too", "profile_photo_id" => "2",
-        "profile_video_id" => "2", "type" => "Candidate"} }.to change(Candidate, :count).by(1)
+        @candidate = Candidate.find_by(email: "scott@gmail.com")
+      end
 
-        response.should redirect_to(candidate_path(Candidate.last))
+      it "redirect to the new canidate's page" do
+        response.should redirect_to(candidate_path(@candidate))
+      end
+
+      it "should set session id" do
+        session[:id].should eq @candidate.id
+      end
+
+
     end
 
-    it "should set session id" do
+    context "invalid params" do
+      before do
+        @params['email'] = nil
+        post 'create', :candidate => @params
+      end
 
-      expect { post 'create', :candidate => { "email" => "scott@gmail.com",
-        "name" => "Scott M", "phone" => "555-233-6541","password" => "Password1",
-        "password_confirmation" => "Password1", "address1" => "224 north illinois",
-        "address2" => "apt 6002", "city" => "Chicago", "state" => "IL", "zip" => "06061",
-        "twitter" => "kevin@twitter.com", "facebook" => "kevin@facebook.com",
-        "linked_in" => "kevin@linked in", "codeacademy" => "badges", "github" => "kevin@github.com",
-        "blog" => "tumblerkevin@tumblr.com", "personal_url" => "kevin@tumblr.com",
-        "currently_working_on" => "codes  academy", "biography" => "from the north side yo",
-        "mission" => "become a great coder too", "profile_photo_id" => "2",
-        "profile_video_id" => "2", "type" => "Candidate"} }.to change(Candidate, :count).by(1)
+      it "should not save the new candidate" do
+        Candidate.find_by(email: "scott@gmail.com").should be_nil
+      end
 
-        session[:id].should eq Candidate.last.id
-    end
-
-    it "should render the new view if the candidate doesnt save" do
-
-      expect { post 'create', :candidate => { "email" => "",
-        "name" => "scott mascio", "phone" => "555-233-6541","password" => "Password1",
-        "password_confirmation" => "Password1", "address1" => "224 north illinois",
-        "address2" => "apt 6002", "city" => "", "state" => "IL", "zip" => "06061",
-        "twitter" => "kevin@twitter.com", "facebook" => "kevin@facebook.com",
-        "linked_in" => "kevin@linked in", "codeacademy" => "badges", "github" => "kevin@github.com",
-        "blog" => "tumblerkevin@tumblr.com", "personal_url" => "kevin@tumblr.com",
-        "currently_working_on" => "codes  academy", "biography" => "from the north side yo",
-        "mission" => "become a great coder too", "profile_photo_id" => "2",
-        "profile_video_id" => "2", "type" => "Candidate"} }.to_not change(Candidate, :count).by(1)
-
-
+      it "should have a flash notice if params are missing" do
+        flash[:error].should_not be_nil
         response.should render_template(:new)
+      end
+
+      it "should render the new view if the candidate doesnt save" do
+        response.should render_template(:new)
+      end
     end
-
-    it "should have a flash notice if params are missing" do
-
-        expect { post 'create', :candidate => { "email" => "",
-          "name" => "scott mascio", "phone" => "555-233-6541","password" => "Password1",
-          "password_confirmation" => "Password1", "address1" => "224 north illinois",
-          "address2" => "apt 6002", "city" => "", "state" => "IL", "zip" => "06061",
-          "twitter" => "kevin@twitter.com", "facebook" => "kevin@facebook.com",
-          "linked_in" => "kevin@linked in", "codeacademy" => "badges", "github" => "kevin@github.com",
-          "blog" => "tumblerkevin@tumblr.com", "personal_url" => "kevin@tumblr.com",
-          "currently_working_on" => "codes  academy", "biography" => "from the north side yo",
-          "mission" => "become a great coder too", "profile_photo_id" => "2",
-          "profile_video_id" => "2", "type" => "Candidate"} }.to_not change(Candidate, :count).by(1)
-
-          flash[:error].should_not be_nil
-          response.should render_template(:new)
-    end
-
   end
+
 
   describe "#new" do
 
@@ -94,6 +70,9 @@ describe CandidatesController do
   end
 
   describe "#show" do
+    before(:each) do
+     @candidate = FactoryGirl.create(:candidate)
+   end
 
     it "should show the candidate profile view" do
 
@@ -115,7 +94,9 @@ describe CandidatesController do
   end
 
   describe "#update" do
-
+    before(:each) do
+     @candidate = FactoryGirl.create(:candidate)
+   end
      it 'should update mission statment for candidate' do
 
       controller.stub(:current_user).and_return(@candidate)
