83b20298de58bc9343c1f7b33979197ca5def51f
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-22 10:18:53 -0500

    updating integration testing

--- spec/features/signup_page_candidate_spec.rb
+++ spec/features/signup_page_candidate_spec.rb
@@ -6,32 +6,28 @@ describe "Sign Up Page for candidate" do
   it "should let me sign up as a candidate" do
     visit root_path
     click_link "Sign Up!"
-    click_link "Interested in applying?"
+    click_link "Interested in applying?", match: :first
 
       fill_in 'Name', with: 'Derrick Rosie'
       fill_in 'candidate_email', with: 'derrickrosie@gmail.com'
-      fill_in 'candidate_phone', with: '123-565-1887'
       fill_in 'candidate_password', with: 'numbertwo'
       fill_in 'candidate_password_confirmation', with: 'numbertwo'
-      fill_in 'candidate_address1', with: 'trump towers'
-      fill_in 'candidate_address2', with: 'englewoods'
-      fill_in 'candidate_city', with: 'chicagoo'
-      fill_in 'candidate_state', with: 'IL'
-      fill_in 'candidate_zip', with: '606001'
+
 
       click_button "Create Profile"
-      page.should have_content "Derrick Rosie"
-      page.should have_content "Profile Page"
+      page.should have_content "Derrick Rosie's Profile"
+      page.should have_content "Mission Statement"
   end
 
   it "should show the copyright" do
     visit root_path
     click_link "Sign Up!"
-    click_link "Interested in applying?"
+    click_link "Interested in applying?", match: :first
 
     within('footer') { expect(page).to have_content('Copyright') }
   end
 end
+
 
 
 
