45fa678f90110c368d54a9ec4f2487aed9b58671
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-22 10:27:40 -0500

    added jquery-ui-rails gem

--- app/assets/javascripts/submission_confirmation.js
+++ app/assets/javascripts/submission_confirmation.js
@@ -1,6 +1,41 @@
-$(document).ready(function(){
-  $('.submit_button').click(function(e){
-    e.preventDefault();
-    console.log('Are you sure you want to submit this?');
+$(function() {
+  $( "#dialog-confirm" ).dialog({
+    autoOpen: false,
+    resizable: false,
+    height:140,
+    modal: true,
+    buttons: {
+      "Confirm Nomination": function() {
+        $( this ).dialog( "close" );
+      },
+      Cancel: function() {
+        $( this ).dialog( "close" );
+      }
+    });
   });
-});
+
+$(document).ready(function(){
+  $('#submit-application').click(function(e){
+    e.preventDefault();
+    $( "#dialog-confirm" ).dialog( "open" );
+    });
+  });
+
+
+
+
+
+
+
+
+//     var urlscript = this.href;  /* read link url (e.g. delete.php?id=314159) */
+
+//     $.messager.alert('Warning','The warning message');
+//     $.messager.confirm('Confirm','Are you sure you want to delete record?',function(r){
+//         if (r) {
+//            $.ajax(urlscript); /* make ajax call to that url with the right id */
+//         }
+//     });
+//  });
+// });
+// });
