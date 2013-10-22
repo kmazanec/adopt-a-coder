67076a345a5f4eed51216ad0ed2336fdb7eae460
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-22 10:18:00 -0500

    updating integration testing

--- spec/support/user.helper.rb
+++ spec/support/user.helper.rb
@@ -3,7 +3,7 @@ module UserHelper
   def login_as_donor
     visit root_path
     click_link "Sign Up!"
-    click_link "Interested in being a supporter?"
+    click_link "Interested in being a supporter?", match: :first
 
       fill_in 'Name', with: 'Scottie'
       fill_in 'donor_email', with: 'scottiepippen@gmail.com'
@@ -16,20 +16,15 @@ module UserHelper
   def login_as_candidate
     visit root_path
     click_link "Sign Up!"
-    click_link "Interested in applying?"
+    click_link "Interested in applying?", match: :first
 
       fill_in 'Name', with: 'Derrick Rose'
       fill_in 'candidate_email', with: 'derrickrose@gmail.com'
-      fill_in 'candidate_phone', with: '123-565-1897'
       fill_in 'candidate_password', with: 'numberone'
       fill_in 'candidate_password_confirmation', with: 'numberone'
-      fill_in 'candidate_address1', with: 'trump tower'
-      fill_in 'candidate_address2', with: 'englewood'
-      fill_in 'candidate_city', with: 'chicago'
-      fill_in 'candidate_state', with: 'IL'
-      fill_in 'candidate_zip', with: '60601'
 
       click_button "Create Profile"
   end
 end
+
 
