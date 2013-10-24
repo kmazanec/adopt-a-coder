385a4c9c9c2be1de2c257b2db7c72ac536431b7f
kmazanec <k.mazanec@gmail.com>
2013-10-24 10:14:21 -0500

    tweaks to various things

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $('.countdown').downCount({
-    date: '10/25/2013 12:00:00',
+    date: '10/31/2013 12:00:00',
     offset: -6
   }, function () {
-    alert('WOOT WOOT, done!');
+    
   });
 });
