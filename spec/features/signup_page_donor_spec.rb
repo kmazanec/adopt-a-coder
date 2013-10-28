075276775fcd343b3846563e3323418cfc415d33
Scott R. Mascio <ScottMascio@Scotts-MacBook-Pro.local>
2013-10-28 14:58:22 -0500

    trying to make travis build correctly

--- spec/features/signup_page_donor_spec.rb
+++ spec/features/signup_page_donor_spec.rb
@@ -4,6 +4,12 @@ include UserHelper
 describe "Sign Up Page for donor" do
   before(:each) do
     c = FactoryGirl.create(:candidate)
+    m = Media.create(url: "Adigo_small.jpg",
+             type: "Image", user_id: c.id)
+    f = Media.create(url: "http://www.youtube.com/watch?v=BU3jfbb172E",
+             type: "Video", user_id: c.id)
+    c.update_attributes(profile_video_id: f.id)
+    c.update_attributes(profile_photo_id: m.id)
     Campaign.create(candidate: c, start_date: "31/10/2013", end_date: "31/12/2013", total: 0, goal: 12500, current_campaign: true)
   end
 
